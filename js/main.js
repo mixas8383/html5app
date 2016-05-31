f17ae6069f484d33a4d85a1a2b81b2b3d331604d
mixas <mixas8383@yahoo.com>
2016-05-31 16:19:44 +0400

    update angulyar version 1.5 and make some timer

--- js/main.js
+++ js/main.js
@@ -1,36 +1,62 @@
-var app = angular.module("game", []);
 
-
-
-t = new Array;
-t.length
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
 var app = angular.module('game', []);
-app.controller('gameCtrl', function($scope) {
-    
-    
-    
-    for(i = 0;i<coords.length;i++)
-    {
-        coords[i].color = getRandomInt(0,255)+', '+getRandomInt(0,255)+', '+getRandomInt(0,255);
-    }
- 
-    Math.random()
-    $scope.items = coords;
-       $scope.sectorClick = function(a,b,c,d)
-       {
-           a.cords = '';
+app.controller('gameCtrl', ['$scope', '$interval', function ($scope, $interval) {
+
+
+
+        for (i = 0; i < coords.length; i++)
+        {
+            coords[i].color = getRandomInt(0, 255) + ', ' + getRandomInt(0, 255) + ', ' + getRandomInt(0, 255);
+        }
+
+        Math.random()
+        $scope.items = coords;
+        $scope.sectorClick = function (a, b, c, d)
+        {
+            a.cords = '';
+
+
+
+        }
+
+
+       
+        $scope.timerCounter = 0;
+      
+
+        var stop;
+        $scope.fight = function () {
+            // Don't start a new fight if we are already fighting
+            if (angular.isDefined(stop))
+                return;
+
+            stop = $interval(function () {
+               $scope.timerCounter++;
+            }, 1000);
+        };
+
+        $scope.stopFight = function () {
+            if (angular.isDefined(stop)) {
+                $interval.cancel(stop);
+                stop = undefined;
+            }
+        };
+
+        $scope.resetFight = function () {
+            $scope.timerCounter = 0;
            
-           console.log(a);
-           console.log(b);
-           console.log(c);
-           console.log(d);
-          
-       }
-});
+        };
+
+        $scope.$on('$destroy', function () {
+            // Make sure that the interval is destroyed too
+            $scope.stopFight();
+        });
+
+    }]);
 
 
