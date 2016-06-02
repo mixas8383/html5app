8608f2d940c822a37575010a64cd412a2ccde708
mixas <mixas8383@yahoo.com>
2016-06-02 14:44:34 +0400

    some gameplay

--- js/main.js
+++ js/main.js
@@ -2,6 +2,80 @@
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+var Gm = function () {
+    this.relations = null;
+    this.state = 0;// o game init 1- game starter 2-game wone 
+    this.currentNumber = 0;
+    this.currentNextNumber = 1;
+
+
+    this.setRelation = function (array)
+    {
+        this.relations = array;
+    }
+
+    this.findNumberIndex = function (i)
+    {
+        return this.relations[i];
+    }
+    this.findSectorIndex = function (k)
+    {
+
+        for (i = 0; i < this.relations.length; i++)
+        {
+            if (k == this.relations[i])
+            {
+                return i;
+            }
+        }
+
+    }
+
+    this.start = function ()
+    {
+        this.state = 1;
+    }
+    this.incrementCounter = function ()
+    {
+        this.currentNumber++;
+        this.currentNextNumber++;
+    }
+
+
+
+
+    this.getRandomize = function ()
+    {
+        var array = new Array();
+        for (i = 0; i < 90; i++)
+        {
+            array[i] = i + 1;
+        }
+
+
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
+
+
+
+};
+
 
 
 var app = angular.module('game', []);
@@ -14,33 +88,66 @@ app.controller('gameCtrl', ['$scope', '$interval', function ($scope, $interval) 
             coords[i].color = getRandomInt(0, 255) + ', ' + getRandomInt(0, 255) + ', ' + getRandomInt(0, 255);
         }
 
-        Math.random()
+        gameClass = new Gm;
+
         $scope.tempFigureIndex = 0;
         $scope.relations = [];
         $scope.items = coords;
-        $scope.texts = texts;
-        $scope.sectorClick = function (a, b, c, d)
-        {
-            a.cords = '';
-            $scope.tempFigureIndex = a.idx;
+        $scope.currentIndex = 0;
+        gameClass.setRelation(relation);
 
+
+        randomed = gameClass.getRandomize();
+        for (i = 0; i < randomed.length; i++)
+        {
+
+            coords[i].gameIndex = randomed[i];
+            texts[gameClass.findNumberIndex(coords[i].idx)].gameIndex = randomed[i];
+            console.log(texts[i].idx)
+            texts[i].text = randomed[i];
 
         }
 
+        $scope.texts = texts;
+
+
+
+
+
+        $scope.sectorClick = function (item)
+        {
+
+            item.cords = '';
+            $scope.tempFigureIndex = item.idx;
+
+            selfIdx = gameClass.findNumberIndex(item.idx);
+            $scope.texts[selfIdx].x = -1;
+            $scope.texts[selfIdx].y = -1;
+            $scope.currentIndex = item.gameIndex;
+
+        }
+
+
+
         $scope.numberClick = function (a, b, c, d)
         {
-            
-            if($scope.tempFigureIndex <0)
-                return;
-            
+
+
+
+
             a.cords = '';
-            $scope.relations[$scope.tempFigureIndex] = a.idx
+
+            sectorIdx = gameClass.findSectorIndex(a.idx);
+
+            $scope.items[sectorIdx].cords = '';
+
+
             a.text = '';
             a.x = -100;
             a.y = -100;
             $scope.tempFigureIndex = -1;
-
-console.log($scope.relations);
+            $scope.currentIndex = a.gameIndex;
+            console.log($scope.relations);
         }
 
 
@@ -76,6 +183,15 @@ console.log($scope.relations);
             $scope.stopFight();
         });
 
+
+
+
+
+
     }]);
+
+
+
+
 
 
